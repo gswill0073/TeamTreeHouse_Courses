82717d999942915a4a6d6adba14a76df5323566e
Greg Williams <50665084+gswill0073@users.noreply.github.com>
2019-08-29 11:22:41 -0400

    Delete introductions.java

--- Java/introductions.java
+++
@@ -1,9 +0,0 @@
-import java.io.Console;
-
-public class Introductons {
-
-    public static void main(String[] args){
-        Console console = System.console();
-        console.printf("Hello, my name is Greg");
-    }
-}
